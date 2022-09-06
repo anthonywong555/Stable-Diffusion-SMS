8d180766db08405811193072e04a9a746cff6f9d
anthonywong555 <anthonywong555@gmail.com>
2022-09-06 00:25:50 -0400

    Add Twilio

--- src/index.js
+++ src/index.js
@@ -8,26 +8,61 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import stability from 'stability-ts';
 import { Bannerbear } from 'bannerbear';
+import twilio from 'twilio';
 
 /**
  * Clients
  */
+const twilioClient = twilio( 
+    process.env.TWILIO_API_KEY, 
+    process.env.TWILIO_API_KEY_SECRET, 
+    { accountSid: process.env.TWILIO_ACCOUNT_SID }
+);
 const bbClient = new Bannerbear(process.env.BANNER_BEAR_API_KEY);
 
 /**
  * Application
  */
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 8080;
 
 const { generate } = stability;
 
 app.post('/sms', async (req, res) => {
     try {
-        const {body} = req;
-        const {text} = body;
-        const generatedImages = await generateStableDiffusionImages(text);
+        const {headers, body = ''} = req;
+        const twilioSignature = headers['x-twilio-signature'];
+        const url = `${process.env.PRODUCTION_URL}/sms`;
+        const requestIsValid = twilio.validateRequest(
+            process.env.TWILIO_AUTH_TOKEN,
+            twilioSignature,
+            url,
+            body
+        );
+        
+        if (process.env.NODE_ENV === 'production') {
+            if(!requestIsValid) {
+                return res.status(403).send('Forbidden');
+            }
+        }
+
+        if(body.Body.length) {
+            res.send('<Response><Message>Processing...</Message></Response>');
+            await driver(body);
+        } else {
+            res.send('<Response><Message>Please send a detailed description of what you want to see.</Message></Response>');
+        }
+    } catch (e) {
+        console.error(`An error has occurred: \n${e}`);
+        return res.status(500).send('Internal Server Error');
+    }
+});
+
+const driver = async (twilioRequest) => {
+    try {
+        const {To, From, Body} = twilioRequest;
+        const generatedImages = await generateStableDiffusionImages(Body);
         const generatedImagesLocalURLs = generatedImages.map((aGeneratedImage) => aGeneratedImage.filePath);
         
         console.log(`generatedImagesLocalURLs: ${generatedImagesLocalURLs}`);
@@ -35,8 +70,8 @@ app.post('/sms', async (req, res) => {
         // Generate BannerBear Promies
         const bannerBearPromies = generatedImagesLocalURLs.map(async(localURLs) => {
             const filePath = localURLs.replace('/home/node/app/', '');
-            const fullURL = `${process.env.BASE_URL}/${filePath}`;
-            return await generateBannerBearImage({fullURL, text});
+            const fullURL = `${process.env.PRODUCTION_BASE_URL}/${filePath}`;
+            return await generateBannerBearImage({fullURL, text: Body});
         });
 
         const bannerBearImages = await Promise.all(bannerBearPromies);
@@ -45,11 +80,23 @@ app.post('/sms', async (req, res) => {
 
         console.log(`bannerBearImageUIDs: ${bannerBearImageUIDs}`);
 
-        res.send(bannerBearImages);
+        // Generate Twilio SMS Promies
+        const TwilioPromies = bannerBearImageURLs.map(async (mediaUrl) => {
+            return await twilioClient.messages.create({
+                mediaUrl,
+                to: From,
+                from: To
+            });
+        });
+
+        const twilioSMSResponses = await Promise.all(TwilioPromies);
+        const twilioSMSSIDs = twilioSMSResponses.map((aSMSResponse) => aSMSResponse.sid);
+        
+        console.log(`twilioSMSSIDs: ${twilioSMSSIDs}`);
     } catch (e) {
         res.send(e);
     }
-});
+}
 
 const generateBannerBearImage = async ({fullURL, text}) => {
     return await bbClient.create_image(process.env.BANNER_BEAR_IMAGE_TEMPLATE_ID, {
