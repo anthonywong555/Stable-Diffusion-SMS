e75d94c2e2a1d5d427cc2f30436192d473cce865
anthonywong555 <anthonywong555@gmail.com>
2022-09-05 22:58:04 -0400

    Add Stable Diffusion Logic

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@
 import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
+import stability from 'stability-ts';
 
 /**
  * Clients
@@ -14,14 +15,48 @@ const app = express();
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
+const { generate } = stability;
+
+app.post('/sms', async (req, res) => {
+    try {
+        const {body} = req;
+        console.log(body);
+        const response = await generateStableDiffusionImage('Shiba Inu wearing a top hat');
+        const filePath = response.filePath.replace('/home/node/app/', '');
+        const fullURL = `${process.env.BASE_URL}/${filePath}`;
+        console.log(fullURL);
+        res.send(fullURL);
+    } catch (e) {
+        res.send(e);
+    }
 });
 
-app.post('/test', (req, res) => {
-    const {body} = req;
-    console.log(`/test received the following payload: `, body);
-    res.send(body);
-});
+const generateBannerBearImage = async ({imageURL, title}) => {
 
+}
+
+const generateStableDiffusionImage = async (prompt) => {
+    return new Promise((resolve, reject) => {
+        const stabilityClient = generate({
+            prompt,
+            apiKey: process.env.DREAMSTUDIO_API_KEY,
+            width: 960,
+            height: 960,
+            samples: 1,
+            outDir: 'public'
+        });
+
+        stabilityClient.on('image', ({buffer, filePath}) => {
+            resolve({buffer, filePath})
+        });
+    
+        stabilityClient.on('end', (response) => {
+            if(!response.isOk) {
+                reject(response);
+            }
+        });        
+    });
+}
+
+app.use('/public', express.static('public'));
 app.listen(PORT, () => console.log(`Listening on ${PORT}.\nNode Environment is on ${process.env.NODE_ENV} mode.`));
